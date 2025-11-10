b9c928ed283ea6f2afff307f1e03813a1ba442a1
Vit0rSilva <vitor.work21@gmail.com>
2025-11-10 00:09:39 -0300

    Atualizando URL

--- aomossar/src/app/api/base/base.ts
+++ aomossar/src/app/api/base/base.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // Sua URL base de API para testes locais
-  apiUrl: 'http://127.0.0.1:8000' 
+  apiUrl: 'https://algumacoisa-python.gdaxye.easypanel.host' 
 };
